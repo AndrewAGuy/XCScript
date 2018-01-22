241adcdb3091cc861074563de6bdf92f78bb4c34
Andrew Guy <andrewguy1995@gmail.com>
2018-01-22 22:11:25 +0000

    master - Plugin functions

---
+++ XCScript/Functions/Plugins/Alias.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using XCScript.Arguments;
+using XCScript.Functions.Exceptions;
+using XCScript.Plugins;
+
+namespace XCScript.Functions.Plugins
+{
+    internal class Alias : IFunction
+    {
+        public string Keyword
+        {
+            get
+            {
+                return "alias";
+            }
+        }
+
+        public object Execute(IArgument[] arguments, Dictionary<string, object> globals)
+        {
+            if (arguments.Length == 0)
+            {
+                throw new ArgumentCountException("'alias' needs at least 1 argument");
+            }
+            else if (arguments.Length > 2)
+            {
+                var res = globals[Engine.RKey] as Result;
+                res.Messages.Add($"'alias' called with {arguments.Length} arguments, only first 2 will be used");
+            }
+
+            var manager = globals[Engine.PKey] as Manager;
+            if (arguments.Length == 1)
+            {
+                if (!(arguments[0].Evaluate(globals) is Dictionary<string, IArgument> dict))
+                {
+                    throw new ArgumentTypeException("'alias' called with 1 argument takes a dictionary");
+                }
+                foreach (var kv in dict)
+                {
+                    if (kv.Value.Evaluate(globals) is string s)
+                    {
+                        manager.Alias(kv.Key, s);
+                    }
+                }
+            }
+            else
+            {
+                if (arguments[0].Evaluate(globals) is string name &&
+                    arguments[1].Evaluate(globals) is string type)
+                {
+                    manager.Alias(name, type);
+                }
+                else
+                {
+                    throw new ArgumentTypeException("'alias' maps 2 strings to each other");
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ XCScript/Functions/Plugins/Property.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using XCScript.Arguments;
+using XCScript.Functions.Exceptions;
+using XCScript.Plugins;
+
+namespace XCScript.Functions.Plugins
+{
+    internal class Property : IFunction
+    {
+        public string Keyword
+        {
+            get
+            {
+                return "prop";
+            }
+        }
+
+        public object Execute(IArgument[] arguments, Dictionary<string, object> globals)
+        {
+            // Variations:
+            //      Get: IPlugin, string => object
+            //      Set: IPlugin, string, object
+            //      Set: IPlugin, dict {string => object}
+            if (arguments.Length < 2)
+            {
+                throw new ArgumentCountException("'prop' requires at least 2 arguments");
+            }
+            else if (arguments.Length > 3)
+            {
+                var res = globals[Engine.RKey] as Result;
+                res.Messages.Add($"'prop' called with {arguments.Length} arguments, only first 3 will be used");
+            }
+
+            var plugin = arguments[0].Evaluate(globals) as IPlugin;
+            if (plugin == null)
+            {
+                throw new ArgumentTypeException("'prop' requires the first argument to be of type IPlugin");
+            }
+
+            var arg1 = arguments[1].Evaluate(globals);
+            if (arg1 is Dictionary<string, IArgument> dict)
+            {
+                foreach (var kv in dict)
+                {
+                    plugin[kv.Key]?.TrySetValue(kv.Value.Evaluate(globals));
+                }
+                return plugin;
+            }
+            else if (arg1 is string name)
+            {
+                if (arguments.Length > 2)
+                {
+                    plugin[name]?.TrySetValue(arguments[2].Evaluate(globals));
+                    return plugin;
+                }
+                else
+                {
+                    return plugin[name]?.Value;
+                }
+            }
+            else
+            {
+                throw new ArgumentTypeException("'prop' requires string or dictionary arguments");
+            }
+        }
+    }
+}
