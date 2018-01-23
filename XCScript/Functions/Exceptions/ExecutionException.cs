d9c6e4e16ef75b5ef220067bc7c769f0e081d255
Andrew Guy <andrewguy1995@gmail.com>
2018-01-23 20:16:46 +0000

    master - Execution exception now public, so custom functions can be policed normally and other exceptions still bubble up

--- XCScript/Functions/Exceptions/ExecutionException.cs
+++ XCScript/Functions/Exceptions/ExecutionException.cs
@@ -2,12 +2,22 @@
 
 namespace XCScript.Functions.Exceptions
 {
-    class ExecutionException : Exception
+    /// <summary>
+    /// Base type for execution exceptions, which are caught by <see cref="Engine"/>
+    /// </summary>
+    public class ExecutionException : Exception
     {
+        /// <summary>
+        /// Default ctor
+        /// </summary>
         public ExecutionException()
         {
         }
 
+        /// <summary>
+        /// Message ctor
+        /// </summary>
+        /// <param name="message"></param>
         public ExecutionException(string message) : base(message)
         {
         }
