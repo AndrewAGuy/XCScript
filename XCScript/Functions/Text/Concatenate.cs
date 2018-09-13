aef8e44594159d21b00280b3d9c4b8117f0fe652
Andrew Guy <andrewguy1995@gmail.com>
2018-09-13 23:05:52 +0100

    master - Basic text manipulation for messaging added

---
+++ XCScript/Functions/Text/Concatenate.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using XCScript.Arguments;
+
+namespace XCScript.Functions.Text
+{
+    internal class Concatenate : IFunction
+    {
+        public string Keyword
+        {
+            get
+            {
+                return "strcat";
+            }
+        }
+
+        public object Execute(IArgument[] arguments, Engine context)
+        {
+            var str = new StringBuilder();
+            foreach (var arg in arguments)
+            {
+                str.Append(arg.Evaluate(context).ToString());
+            }
+            return str;
+        }
+    }
+}
---
+++ XCScript/Functions/Text/Message.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using XCScript.Arguments;
+
+namespace XCScript.Functions.Text
+{
+    internal class Message : IFunction
+    {
+        public string Keyword
+        {
+            get
+            {
+                return "msg";
+            }
+        }
+
+        private Dictionary<string, int> codes = new Dictionary<string, int>()
+        {
+            { "information", Engine.Information },
+            { "info", Engine.Information },
+            { "i", Engine.Information },
+            { "error", Engine.Error },
+            { "err", Engine.Error },
+            { "e", Engine.Error },
+            { "warning", Engine.Warning },
+            { "warn", Engine.Warning },
+            { "w", Engine.Warning }
+        };
+
+        public object Execute(IArgument[] arguments, Engine context)
+        {
+            if (arguments.Length == 0)
+            {
+                context.Log(null); // Sends a timestamp only
+            }
+
+            var code = Engine.Information;
+            if (arguments.Length > 1)
+            {
+                switch (arguments[1].Evaluate(context))
+                {
+                    case int i:
+                        code = i;
+                        break;
+                    case string s:
+                        if (codes.TryGetValue(s.ToLower(), out var c))
+                        {
+                            code = c;
+                        }
+                        break;
+                }
+            }
+
+            context.Log(arguments[0].Evaluate(context).ToString(), code);
+            return null;
+        }
+    }
+}
