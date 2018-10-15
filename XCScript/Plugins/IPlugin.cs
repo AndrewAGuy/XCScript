5a52f1eeab9da98d75ad456dead0f1a1d46088fe
Andrew Guy <andrewguy1995@gmail.com>
2018-10-15 13:55:36 +0100

    master - Plugins are now property enumerable.

--- XCScript/Plugins/IPlugin.cs
+++ XCScript/Plugins/IPlugin.cs
@@ -1,11 +1,12 @@
-﻿using XCScript.Plugins.Interaction;
+﻿using System.Collections.Generic;
+using XCScript.Plugins.Interaction;
 
 namespace XCScript.Plugins
 {
     /// <summary>
     /// Represents a type that can be loaded dynamically, and modified using <see cref="IProperty"/>
     /// </summary>
-    public interface IPlugin
+    public interface IPlugin : IEnumerable<IProperty>
     {
         /// <summary>
         /// Gets the property with the given name, or null
--- XCScript/Plugins/PluginBase.cs
+++ XCScript/Plugins/PluginBase.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using XCScript.Plugins.Interaction;
 
@@ -7,12 +8,35 @@ namespace XCScript.Plugins
     /// <summary>
     /// Useful base implementation for <see cref="IPlugin"/>
     /// </summary>
-    public class PluginBase
+    public class PluginBase : IEnumerable
     {
         /// <summary>
         /// Property set, as accessible by general purpose users
         /// </summary>
         protected Dictionary<string, IProperty> Properties { get; private set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerator<IProperty> GetEnumerator()
+        {
+            return this.Properties.Values.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="property"></param>
+        protected void Add(IProperty property)
+        {
+            this.Properties[property.Name] = property;
+        }
 
         /// <summary>
         /// Defaults to blank set
